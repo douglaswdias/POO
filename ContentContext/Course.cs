491acb3fc245a1d18828503d8ecd2c392aea0784
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-01-19 22:13:16 -0300

    add Module.cs and Lecture.cs

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,7 +1,17 @@
+using System.Net.Http.Headers;
+using POO.ContentContext.Enums;
+
 namespace POO.ContentContext
 {
   public class Course : Content
   {
-
+    public Course()
+    {
+      Modules = new List<Module>();      
+    }
+    public string? Tag { get; set; }
+    public IList<Module> Modules { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EcontentLevel Level { get; set; }
   }
 }
---
+++ ContentContext/Enums/EContentLevel.cs
@@ -0,0 +1,10 @@
+namespace POO.ContentContext.Enums
+{
+  public enum EcontentLevel
+  {
+    Beginner = 1,
+    Fundamental = 2,
+    Intermidiate = 3,
+    Advance= 4
+  }
+}
